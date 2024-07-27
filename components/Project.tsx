74f082596e9888b61a5b2e36f4f4685b65af4c2f
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-07-27 19:47:59 +0000

    Improving the style on small screens

--- components/Project.tsx
+++ components/Project.tsx
@@ -19,13 +19,13 @@ function Project({ children, title, src, github } : { children: ReactNode, title
 
 
     return (
-        <div className="min-h-96 m-6 grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center text-base lg:text-lg">
+        <div className="min-h-96 lg:m-6 grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center text-base lg:text-lg">
             {(num % 2 == 0) && <video className="rounded-lg m-5" controls>
                 <source src={src} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>}
             <div className="col-span-1 p-0 lg:p-5 m-2">
-                <h1 className="text-center text-xl font-bold my-2">{title}</h1>
+                <h1 className="text-center text-lg lg:text-xl font-bold my-2">{title}</h1>
                 <div className="text-justify">
                     {children}
                 </div>
