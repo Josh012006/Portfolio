46c8e9f1126227d785c9ccc97b147af53e80cf62
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-11-25 16:23:03 -0500

    Rearranging my experiences

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -33,6 +33,13 @@ function ExperiencePage() {
             <h1 className="text-center text-3xl lg:text-4xl py-10">My Experience</h1>
             <p className="text-center my-2 text-sm lg:text-lg">I&apos;ve had many experiences since I started programming and particularly web development.</p>
             <div className="p-0 lg:p-5">
+                <Experience title="Internship in fullstack development at Prodigy InfoTech" logo="prodigyinfotech.png" period="01 August 2024 - 31 August 2024 (1 month)" companyInfos="Prodigy InfoTech(Mumbai - 400018, Maharashta, India)" site="https://prodigyinfotech.dev/" mail="contact@prodigyinfotech.dev">
+                    I did a one month internship at Prodigy InfoTech as a remote fullstack web developer. My tasks included the development of: i. a user authentication system with secure login and registration functionality, ii. an employee management system, specifically a web application that allows 
+                    administrators to perform CRUD (Create, Read, Update, Delete) operations on employee records, iii. a real-time chat application using WebSocket technology to enable instant messaging between users, and iv. a social media platform. I learned a lot and had the opportunity to experiment 
+                    more with WebSockets and JWT. All my projects are displayed on my linkedin as posts so you can go and check it.
+                    <br />
+                    As a reward for my outstanding performances, I received a <Link className="text-blue-700 underline" target="_blank" href="/experience/Certificate_of_Completion-Prodigy_InfoTech.pdf">certificate of completion</Link> and a <Link className="text-blue-700 underline" target="_blank" href="/experience/Letter_of_Recommendation-Prodigy_InfoTech.pdf">letter of recommendation</Link> from the company.
+                </Experience>
                 <Experience title="Software Development Bootcamp at GOMYCODE" logo="gomycode.jpeg" period="28 January 2024 - 13 July 2024 (6 months)" companyInfos="GomyCode Sénégal" site="https://gomycode.com/home-01/" mail="hello@gomycode.com">
                     I participated in a 6 months bootcamp at GOMYCODE SENEGAL. There I learned all there was to know about fullstack web development. We started with the basics like HTML, CSS (with Bootstrap, TailwindCSS and also SASS) and JavaScript (with the DOM and ES6). We then learned how to use Git and Github for Version Control. 
                     Then next step was to build a solid foundation in Algorithms. So we explored data structures, recursion, sorting and searching algorithms and worked mainly on our logic and our critical thinking. After that we dived into relational databases with SQL (DDL, DML, DQL and MySQL) and non-relational databases focusing mainly on MongoDB and Mongoose. 
@@ -44,13 +51,6 @@ function ExperiencePage() {
                     <br />
                     At the end of the bootcamp, I received a certification in fullstack web development for my dedication and to prove my knowledge.
                     You can check it <Link className="text-blue-700 underline" target="_blank" href="https://diploma.gomycode.app/?id=31722254428800479">here</Link>.
-                </Experience>
-                <Experience title="Internship in fullstack development at Prodigy InfoTech" logo="prodigyinfotech.png" period="01 August 2024 - 31 August 2024 (1 month)" companyInfos="Prodigy InfoTech(Mumbai - 400018, Maharashta, India)" site="https://prodigyinfotech.dev/" mail="contact@prodigyinfotech.dev">
-                    I did a one month internship at Prodigy InfoTech as a remote fullstack web developer. My tasks included the development of: i. a user authentication system with secure login and registration functionality, ii. an employee management system, specifically a web application that allows 
-                    administrators to perform CRUD (Create, Read, Update, Delete) operations on employee records, iii. a real-time chat application using WebSocket technology to enable instant messaging between users, and iv. a social media platform. I learned a lot and had the opportunity to experiment 
-                    more with WebSockets and JWT. All my projects are displayed on my linkedin as posts so you can go and check it.
-                    <br />
-                    As a reward for my outstanding performances, I received a <Link className="text-blue-700 underline" target="_blank" href="/experience/Certificate_of_Completion-Prodigy_InfoTech.pdf">certificate of completion</Link> and a <Link className="text-blue-700 underline" target="_blank" href="/experience/Letter_of_Recommendation-Prodigy_InfoTech.pdf">letter of recommendation</Link> from the company.
                 </Experience>
             </div>
         </section>
