e26a04d907f8c27adc8fcbd4021b9db1828c8aba
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2025-02-18 22:39:28 -0500

    Trying to optimize the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import ReduxProvider from "@/redux/Provider";
@@ -47,6 +48,7 @@ export default function RootLayout({
           <GoUp />
           <Footer />
           <Analytics/>
+          <SpeedInsights/>
         </body>
       </ReduxProvider>
     </html>
